d6b6f089da668c72ca20aa02313e7b2ca224aa42
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-04-19 11:30:11 -0700

    feat: integration footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,17 @@ import React from "react";
 
 import { Link, Route, Switch, useLocation } from "react-router-dom";
 import { Container } from "@mui/material";
-import { HomePage } from "./screens/homePage";
-import { ProductsPage } from "./screens/productsPage";
-import { OrdersPage } from "./screens/ordersPage";
-import { UserPage } from "./screens/userPage";
-import { HomeNavbar } from "./components/headers/HomeNavbar";
-import { OtherNavbar } from "./components/headers/OtherNavbar";
-import { Footer } from "./components/footer";
+import HomePage from "./screens/homePage";
+import ProductsPage  from "./screens/productsPage";
+import  OrdersPage  from "./screens/ordersPage";
+import  UserPage  from "./screens/userPage";
+import  HomeNavbar  from "./components/headers/HomeNavbar";
+import  OtherNavbar  from "./components/headers/OtherNavbar";
+import Footer from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
-import { HelpPage } from "./screens/helpPage";
+import "../css/footer.css";
+import HelpPage from "./screens/helpPage";
 
 
 
