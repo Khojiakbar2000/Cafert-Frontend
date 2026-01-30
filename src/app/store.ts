5175b08b5b17adaa1065253c3ac56896a26c4ada
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2026-01-31 00:06:42 +0900

    fixed again

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,14 +3,17 @@ import reduxLogger from "redux-logger";
 import ProductsPageReducer from "./screens/productsPage/slice";
 import OrdersPageReducer from "./screens/ordersPage/slice";
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 export const store = configureStore({
-  middleware: (getDefaultMiddleware) =>
-    //@ts-ignore
-    getDefaultMiddleware().concat(
-      process.env.NODE_ENV === 'development' 
-        ? reduxLogger 
-        : (() => (next: any) => (action: any) => next(action))()
-    ),
+  middleware: (getDefaultMiddleware) => {
+    const middleware = getDefaultMiddleware();
+    if (isDevelopment) {
+      //@ts-ignore
+      return middleware.concat(reduxLogger);
+    }
+    return middleware;
+  },
   reducer: {
     productsPage: ProductsPageReducer,
     ordersPage: OrdersPageReducer
